d5f1ea67e64370552265717caded2ac2c51e913a
Frenetic Furry! <158255865+FreneticFurry@users.noreply.github.com>
2024-06-15 19:07:33 -0700

    Update FreneticAntiCheat.cs

--- FreneticAntiCheat.cs
+++ FreneticAntiCheat.cs
@@ -9,9 +9,27 @@ public class FreneticAntiCheat : UdonSharpBehaviour
     public float detectionProtection = 2; // alot like Minecrafts Spawn protection
     public float maxSpeed = 5.7f; // maximum allowed speed! VRChat default runspeed is 4 so i ahve it at 5.5 for vr users because sometimes they may move abit irl & arnt trying to cheat (recommended: test this inside the vrchat application because the ClientSim player is different from that actual vrchat player!)
     public float maxOVRAdvancedHeight = 0.9f;
+    public bool autoMaxSpeed = false;
+    public bool autoMaxOVRHeight = false;
+    private float maxJumpHeight;
     private Vector3 previousPosition;
     private Vector3 velocity;
     private float timer = 0f;
+    private Vector3 middlepoint = new Vector3(0f, 0.6625f, 0f); // this is for the height of 1.3 so change it accordingly to match the middle of the height!
+    // attempted calculation (may not work for all speeds or heights so you may want to turn it off.)
+    private void Start()
+    {
+        if (autoMaxSpeed == true)
+        {
+            maxSpeed = Networking.LocalPlayer.GetRunSpeed() * 1.6f;
+        }
+        if (autoMaxOVRHeight == true)
+        {
+            maxOVRAdvancedHeight = Networking.LocalPlayer.GetAvatarEyeHeightAsMeters() * 0.6978f;
+            middlepoint = new Vector3(0f, Networking.LocalPlayer.GetAvatarEyeHeightAsMeters() * 0.51f, 0f);
+        }
+    }
+
     // Anti Cheat System
     private void Update()
     {
@@ -22,7 +40,7 @@ public class FreneticAntiCheat : UdonSharpBehaviour
 
             // Detection Protection
 
-            if (Vector3.Distance(Networking.LocalPlayer.GetPosition() + new Vector3(0f, 0.6625f, 0f), detectionPoint) >= detectionProtection)
+            if (Vector3.Distance(Networking.LocalPlayer.GetPosition() + middlepoint, detectionPoint) >= detectionProtection)
             {
 
                 //Speed Detection
@@ -42,7 +60,7 @@ public class FreneticAntiCheat : UdonSharpBehaviour
                         Networking.LocalPlayer.SetVelocity(Vector3.zero);
                     }
 
-                    if (Networking.LocalPlayer.GetVelocity().y > Networking.LocalPlayer.GetJumpImpulse()*3) // this detects flying "most" of the time but players can still get around it by going slowly so i suggest using colliders to limit areas to prevent them from getting into places they shouldn't.
+                    if (Networking.LocalPlayer.GetVelocity().y > Networking.LocalPlayer.GetJumpImpulse() * 0.9) // this detects flying "most" of the time but players can still get around it by going slowly so i suggest using colliders to limit areas to prevent them from getting into places they shouldn't.
                     {
                         Networking.LocalPlayer.TeleportTo(detectionPoint, new Quaternion(0, 0, 0, 0), VRC_SceneDescriptor.SpawnOrientation.Default, false);
                         Networking.LocalPlayer.SetVelocity(Vector3.zero);
@@ -63,7 +81,10 @@ public class FreneticAntiCheat : UdonSharpBehaviour
                         {
                             switch (collider.gameObject.name)
                             {
-                                case "Player": case "player": case "Bounding Box": case "bounding box": // excluded colliders so you can still use triggers or make triggers with colliders
+                                case "Player":
+                                case "player":
+                                case "Bounding Box":
+                                case "bounding box": // excluded colliders so you can still use triggers or make triggers with colliders
                                     break;
                                 default:
                                     Networking.LocalPlayer.TeleportTo(detectionPoint, new Quaternion(0, 0, 0, 0), VRC_SceneDescriptor.SpawnOrientation.Default, false);
@@ -76,7 +97,7 @@ public class FreneticAntiCheat : UdonSharpBehaviour
 
                 // Distance detection (prevention for OVR advanced abuse :D )
 
-                if (Vector3.Distance(Networking.LocalPlayer.GetPosition() + new Vector3(0f, 0.6625f, 0f), localPlayerCameraPosition) >= maxOVRAdvancedHeight)
+                if (Vector3.Distance(Networking.LocalPlayer.GetPosition() + middlepoint, localPlayerCameraPosition) >= maxOVRAdvancedHeight)
                 {
                     Networking.LocalPlayer.TeleportTo(detectionPoint, new Quaternion(0, 0, 0, 0), VRC_SceneDescriptor.SpawnOrientation.Default, false);
                     Networking.LocalPlayer.SetVelocity(Vector3.zero);
