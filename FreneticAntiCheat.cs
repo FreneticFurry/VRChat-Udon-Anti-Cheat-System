4e46f097b146388161b3e9b8cb7011176088a74a
Frenetic Furry! <158255865+FreneticFurry@users.noreply.github.com>
2024-06-15 18:13:56 -0700

    Update FreneticAntiCheat.cs

--- FreneticAntiCheat.cs
+++ FreneticAntiCheat.cs
@@ -1,4 +1,5 @@
 // VRChat anti cheat system by: FreneticFurry!
+using System.Collections;
 using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
@@ -8,29 +9,49 @@ public class FreneticAntiCheat : UdonSharpBehaviour
     public float detectionProtection = 5; // alot like Minecrafts Spawn protection
     public float maxSpeed = 5.5f; // maximum allowed speed! VRChat default runspeed is 4 so i ahve it at 5.5 for vr users because sometimes they may move abit irl & arnt trying to cheat (recommended: test this inside the vrchat application because the ClientSim player is different from that actual vrchat player!)
     public float maxOVRAdvancedHeight = 0.9f;
-
+    private Vector3 previousPosition;
     // Anti Cheat System
-    private void LateUpdate() // can be changed to Update or FixedUpdate but it wouldn't really make a difference afaik so i suggest leaving it
+    private void Update()
     {
         {
-            if (Vector3.Distance(Networking.LocalPlayer.GetPosition() + new Vector3(0f, 0.6625f, 0f), detectionPoint) >= detectionProtection) // protection so it doesnt constantly go off ounce a player has already been detected for attempting to cheat (like using a collider to fly around or menu bugging)
+            // Variables
+
+            Vector3 localPlayerCameraPosition = Networking.LocalPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head).position;
+            Vector3 velocity;
+
+            // Detection Protection
+
+            if (Vector3.Distance(Networking.LocalPlayer.GetPosition() + new Vector3(0f, 0.6625f, 0f), detectionPoint) >= detectionProtection)
             {
-                // Speed Detection (this is gonna affect everyone! prevents players from doing real cheaty things :)  ) (this is also not very good at doing its job so ima be reworking it at some point)
 
-                Vector3 localPlayerCameraPosition = Networking.LocalPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head).position;
+                //Speed Detection
 
-                if (Networking.LocalPlayer.GetVelocity().x >= maxSpeed || Networking.LocalPlayer.GetVelocity().x <= -maxSpeed)
+                float timer = 0f;
+                timer += Time.deltaTime;
+
+                if (timer >= 0.05f)
                 {
-                    Networking.LocalPlayer.TeleportTo(detectionPoint, new Quaternion(0, 0, 0, 0), VRC_SceneDescriptor.SpawnOrientation.Default, false);
-                    Networking.LocalPlayer.SetVelocity(Vector3.zero);
-                }
-                if (Networking.LocalPlayer.GetVelocity().z >= maxSpeed || Networking.LocalPlayer.GetVelocity().z <= -maxSpeed)
-                {
-                    Networking.LocalPlayer.TeleportTo(detectionPoint, new Quaternion(0, 0, 0, 0), VRC_SceneDescriptor.SpawnOrientation.Default, false);
-                    Networking.LocalPlayer.SetVelocity(Vector3.zero);
+                    timer = 0f;
+
+                    Vector3 amount = localPlayerCameraPosition - previousPosition;
+                    velocity = amount / 0.05f;
+
+                    if (new Vector3(velocity.x, 0f, velocity.z).magnitude > maxSpeed)
+                    {
+                        Networking.LocalPlayer.TeleportTo(detectionPoint, new Quaternion(0, 0, 0, 0), VRC_SceneDescriptor.SpawnOrientation.Default, false);
+                        Networking.LocalPlayer.SetVelocity(Vector3.zero);
+                    }
+
+                    if (Networking.LocalPlayer.GetVelocity().y > Networking.LocalPlayer.GetJumpImpulse()*1.5) // this detects flying "most" of the time but players can still get around it by going slowly so i suggest using colliders to limit areas to prevent them from getting into places they shouldn't.
+                    {
+                        Networking.LocalPlayer.TeleportTo(detectionPoint, new Quaternion(0, 0, 0, 0), VRC_SceneDescriptor.SpawnOrientation.Default, false);
+                        Networking.LocalPlayer.SetVelocity(Vector3.zero);
+                    }
+
+                    previousPosition = localPlayerCameraPosition;
                 }
 
-                // Collider detection (this likely wont affect desktop players but in some cases can still affect them such as if you make bounding boxes)
+                // Collider detection (this likely wont affect desktop players but in some cases can still affect them such as if you make bounding boxes/ triggers)
 
                 Collider[] colliders = Physics.OverlapSphere(localPlayerCameraPosition, 0.1f);
 
@@ -40,13 +61,14 @@ public class FreneticAntiCheat : UdonSharpBehaviour
                     {
                         if (Vector3.Distance(localPlayerCameraPosition, collider.ClosestPointOnBounds(localPlayerCameraPosition)) <= 0)
                         {
-                            if (collider.gameObject.name == "BoundingBox" || collider.gameObject.name == "Bounding Box") // this is the exclusion area where you can exclude things that you may want to use something for other things instead
+                            switch (collider.gameObject.name)
                             {
-                            }
-                            else
-                            {
-                                Networking.LocalPlayer.TeleportTo(detectionPoint, new Quaternion(0, 0, 0, 0), VRC_SceneDescriptor.SpawnOrientation.Default, false);
-                                Networking.LocalPlayer.SetVelocity(Vector3.zero);
+                                case "Player": case "player": case "Bounding Box": case "bounding box": // excluded colliders so you can still use triggers or make triggers with colliders
+                                    break;
+                                default:
+                                    Networking.LocalPlayer.TeleportTo(detectionPoint, new Quaternion(0, 0, 0, 0), VRC_SceneDescriptor.SpawnOrientation.Default, false);
+                                    Networking.LocalPlayer.SetVelocity(Vector3.zero);
+                                    break;
                             }
                         }
                     }
@@ -59,6 +81,11 @@ public class FreneticAntiCheat : UdonSharpBehaviour
                     Networking.LocalPlayer.TeleportTo(detectionPoint, new Quaternion(0, 0, 0, 0), VRC_SceneDescriptor.SpawnOrientation.Default, false);
                     Networking.LocalPlayer.SetVelocity(Vector3.zero);
                 }
+            }
+            else
+            {
+                velocity = new Vector3(0, 0, 0); // resets velocity after being detected so it wont falsely trigger ounce outside of the detection protection properly
+                previousPosition = localPlayerCameraPosition;
             }
         }
     }
