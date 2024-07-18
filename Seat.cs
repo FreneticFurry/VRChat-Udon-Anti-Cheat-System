7cc97939cf532c62570388e538fd4ffb50027229
Frenetic Furry! <158255865+FreneticFurry@users.noreply.github.com>
2024-07-18 03:11:34 -0700

    Added Seat Code
    Compatibility for seats

---
+++ Seat.cs
@@ -0,0 +1,20 @@
+﻿
+using UdonSharp;
+using VRC.SDKBase;
+
+public class Seat : UdonSharpBehaviour
+{
+    public FreneticAntiCheat antiCheat;
+
+    // would make this do something in Start() but i dont know of a method to get the AntiCheat if its found in the scene so you'll just have to manually do it :)
+
+    public override void OnStationEntered(VRCPlayerApi player)
+    {
+        antiCheat.Seat();
+    }
+
+    public override void OnStationExited(VRCPlayerApi player)
+    {
+        antiCheat.Seat();
+    }
+}
